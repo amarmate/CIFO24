1b5486ecd267ea4839a65f01f1952ae950db9702
Mateus Amaral <mbaptistaamaral@gmail.com>
2024-05-01 18:26:33 +0100

    changed some functions

--- sudoku.py
+++ sudoku.py
@@ -16,47 +16,55 @@ class sudoku_game:
         self.difficulty = difficulty
         self.N = dimension
         self.seed = seed
+        self.board = np.zeros((self.N, self.N), dtype=int)
+        np.random.seed = seed
 
+    def generate(self):
+        """
+        Function to construct the board
+        """
+        # Fill the board
+        self.fill_diagonals()
+        self.fill_remaining()
 
+        # Remove numbers from the board
+        self.remove_numbers()
 
-    # [TO DO] -------------------------------------------------------------------------------------
+    def __getitem__(self, position):
+        assert len(position) == 2, "The position has to be a tuple of length 2"
+        return self.board[position]
+    
+    def __setitem__(self, position, value):
+        assert len(position) == 2, "The position has to be a tuple of length 2"
+        self.board[position] = value
+
+    def __repr__(self):
+        return str(self.board)
+
+    # -------------------------- Board construction functions -----------------------------------
+
 
     def fill_diagonals(self):
         """
         Function to fill the diagonal blocks of the board, which are the easiest to fill
         """
-        pass
+        numbers = np.arange(1, self.N + 1)
+        box_size = int(np.sqrt(self.N))
+        box_order = box_size * np.arange(1, box_size + 1) - 1
+        np.random.shuffle(box_order)
 
-    def fill_remaining(self):
-        """
-        Function to fill the remaining cells of the board
-        """
-        pass
+        for i in range(box_size):
+            box = box_order[i]
+            box_coordinates = self.get_box_coordinates((box, box))
+            np.random.shuffle(numbers)
+            for j in range(self.N):
+                self.board[box_coordinates[j]] = numbers[j]
 
-    def check_unused_box(self, position, number):
-        """
-        Function to check if a number can be placed in a certain box
-        :param position: a tuple representing the position of the box
-        :param number: an int representing the number to check
-        """
-        assert len(position) == 2, "The position has to be a tuple of length 2"
-        pass
+    
 
-    def check_unused_row(self, row, number):
-        """
-        Function to check if a number can be placed in a certain row
-        :param row: an int representing the row to check, from left to right
-        :param number: an int representing the number to check
-        """
-        pass
 
-    def check_unused_column(self, column, number):
-        """
-        Function to check if a number can be placed in a certain column
-        :param column: an int representing the column to check, from top to bottom
-        :param number: an int representing the number to check
-        """
-        pass
+
+    # ------------------------- Check functions --------------------------------------------------
 
     def check_is_safe(self, position, number):
         """
@@ -67,9 +75,70 @@ class sudoku_game:
         assert len(position) == 2, "The position has to be a tuple of length 2"
         return self.check_unused_box(position, number) and self.check_unused_row(position[0], number) and self.check_unused_column(position[1], number)
     
-    def generate_board(self):
+
+    def check_unused_box(self, position, number):
         """
-        Function to generate a random sudoku board
+        Function to check if a number can be placed in a certain box
+        :param position: a tuple representing the position of the box
+        :param number: an int representing the number to check
+        """
+        assert number > 0 and number <= self.N, "The number has to be between 1 and N"
+        
+        box_coordinates = self.get_box_coordinates(position)
+        return number not in self.board[box_coordinates]
+    
+
+    def check_unused_row(self, position, number):
+        """
+        Function to check if a number can be placed in a certain row
+        :param position: a tuple representing the position of the number
+        :param number: an int representing the number to check
+        """
+        assert number > 0 and number <= self.N, "The number has to be between 1 and N"
+        row_coordinates = [(position, i) for i in range(self.N)]
+        return number not in self.board[row_coordinates]
+    
+
+    def check_unused_column(self, position, number):
+        """
+        Function to check if a number can be placed in a certain column
+        :param position: a tuple representing the position of the number
+        :param number: an int representing the number to check
+        """
+        assert number > 0 and number <= self.N, "The number has to be between 1 and N"
+        column_coordinates = [(i, position) for i in range(self.N)]
+        return number not in self.board[column_coordinates]
+    
+
+
+    # ------------------------ Other functions ---------------------------------------------------
+
+    def get_box_coordinates(self, position) -> list:
+        """
+        Function to get the coordinates of the box that the position is in. Returns a list of tuples
+        :param position: a tuple representing the position of the box
+        """
+        assert len(position) == 2, "The position has to be a tuple of length 2"
+        assert position[0] < self.N and position[1] < self.N, "The position has to be within the board"
+        box_size = int(np.sqrt(self.N))
+        row, column = position
+        box_row = row // box_size
+        box_column = column // box_size
+        return [(box_row * box_size + i, box_column * box_size + j) for i in range(box_size) for j in range(box_size)]
+    
+
+    # [TO DO] -------------------------------------------------------------------------------------
+
+    def fill_remaining(self):
+        """
+        Function to fill the remaining cells of the board
+        """
+        pass
+    
+
+    def remove_numbers(self):
+        """
+        Function to remove numbers from the board to create a puzzle
         """
         pass
 
